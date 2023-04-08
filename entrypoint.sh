ebace1467c9faa3bd7bbe5d9e209d81b61fa8219
Velnbur <kyrylo.baybula@distributedlab.com>
2023-04-08 17:20:36 +0300

    Add tag name

--- entrypoint.sh
+++ entrypoint.sh
@@ -15,6 +15,7 @@ DESTINATION_REPOSITORY_USERNAME="${8}"
 TARGET_BRANCH="${9}"
 COMMIT_MESSAGE="${10}"
 TARGET_DIRECTORY="${11}"
+TAG_NAME="${12}"
 
 if [ -z "$DESTINATION_REPOSITORY_USERNAME" ]
 then
@@ -150,3 +151,9 @@ git diff-index --quiet HEAD || git commit --message "$COMMIT_MESSAGE"
 echo "[+] Pushing git commit"
 # --set-upstream: sets de branch when pushing to a branch that does not exist
 git push "$GIT_CMD_REPOSITORY" --set-upstream "$TARGET_BRANCH"
+
+echo "[+] Creating a tag"
+git tag "$TAG_NAME"
+
+echo "[+] Pushing tag"
+git push "$GIT_CMD_REPOSITORY" "$TAG_NAME"
